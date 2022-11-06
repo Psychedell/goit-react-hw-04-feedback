ccdfd7e37ee8f0cb3ac80d814a019c4f249fd5c4
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-11-06 18:40:00 +0200

    delete class app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import { Wrapper } from './App.styled';
-import { useEffect } from 'react';
 
 export function App() {
   const [good, setGood] = useState(0);
@@ -61,56 +60,3 @@ export function App() {
     </Wrapper>
   );
 }
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrement = e => {
-//     this.setState(prevState => ({ [e]: prevState[e] + 1 }));
-//   };
-
-//   countTotalFeedback() {
-//     const { good, neutral, bad } = this.state;
-//     let total = 0;
-//     total += good + neutral + bad;
-//     return total;
-//   }
-
-//   countPositiveFeedbackPercentage = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good === 0 ? 0 : Math.round((good / (good + neutral + bad)) * 100);
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const keys = Object.keys(this.state);
-
-//     return (
-//       <Wrapper>
-//         <Section title="Please leave fedback">
-//           <FeedbackOptions
-//             options={keys}
-//             onLeaveFeedback={this.handleIncrement}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </Wrapper>
-//     );
-//   }
-// }
